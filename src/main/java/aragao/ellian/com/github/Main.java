7d005a47c3f1f5167077d22c1de15cc6b0827e30
ellian-aragao <ellian.aragao@gmail.com>
2022-03-13 15:01:52 -0300

    using builder pattern

--- src/main/java/aragao/ellian/com/github/Main.java
+++ src/main/java/aragao/ellian/com/github/Main.java
@@ -1,6 +1,10 @@
 package aragao.ellian.com.github;
 
-import java.math.BigDecimal;
+import aragao.ellian.com.github.models.Cliente;
+import aragao.ellian.com.github.models.Item;
+import aragao.ellian.com.github.models.Vendas;
+import aragao.ellian.com.github.models.Vendedor;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,26 +25,38 @@ public class Main {
 		listStrings.forEach(dataToParse -> {
 			System.out.println(dataToParse);
 			final var strings = dataToParse.split(delimitador);
-			if (Vendedor.id.equals(strings[0])) {
-				final var vendedor = new Vendedor(strings[1], strings[2], new BigDecimal(strings[3]));
+			if (Vendedor.getId().equals(strings[0])) {
+				final var vendedor = Vendedor.builder()
+						.withCpf(strings[1])
+						.withName(strings[2])
+						.withSalary(strings[3])
+						.build();
 				System.out.println(vendedor);
 			}
-			if (Cliente.id.equals(strings[0])) {
-				final var cliente = new Cliente(strings[1], strings[2], strings[3]);
+			if (Cliente.getId().equals(strings[0])) {
+				final var cliente = Cliente.builder()
+						.withCnpj(strings[1])
+						.withName(strings[2])
+						.withBusinessArea(strings[3])
+						.build();
 				System.out.println(cliente);
 			}
-			if (Vendas.id.equals(strings[0])) {
+			if (Vendas.getId().equals(strings[0])) {
 				final var items = Arrays.stream(strings[2].replaceAll("[\\[\\]]", "").split(","))
 						.map(item -> {
 							final var itensAttributes = item.split("-");
-							return new Item(
-									itensAttributes[0],
-									Integer.parseInt(itensAttributes[1]),
-									new BigDecimal(itensAttributes[2])
-							);
+							return Item.builder()
+									.withId(itensAttributes[0])
+									.withQuantity(itensAttributes[1])
+									.withPrice(itensAttributes[2])
+									.build();
 						})
 						.toList();
-				final var vendas = new Vendas(strings[1], items, strings[3]);
+				final var vendas = Vendas.builder()
+						.withSaleId(strings[1])
+						.withItems(items)
+						.withSalesmanName(strings[3])
+						.build();
 				System.out.println(vendas);
 			}
 		});
